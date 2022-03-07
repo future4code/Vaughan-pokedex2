85c1ac2110f253a2016b5705c66485fa51382ac5
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-03-07 14:54:35 -0300

    Router pronto

---
+++ pokedex-2/src/routes/Router.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from '../pages/Home'
+import Pokedex from '../pages/Pokedex'
+import PokemonDetails from '../pages/PokemonDetails'
+import ErrorPage from '../pages/ErrorPage'
+
+const Router = () => {
+    return (<BrowserRouter>
+        <Routes>
+            <Route exact path={"/"} element={<Home />} />
+
+            <Route exact path={"/pokedex"} element={<Pokedex />} />
+
+            <Route exact path={"/pokemon/:id"} element={<PokemonDetails />} />
+
+            <Route exact path={"*"} element={<ErrorPage />} />
+        </Routes>
+    </BrowserRouter>)
+}
+
+export default Router
