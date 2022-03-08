045763863b883b9e2979fbe6c383a6695d78b949
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-03-08 18:23:50 -0300

    pokemon detail - stats

--- pokedex-2/src/pages/PokemonDetails/PokemonDetails.js
+++ pokedex-2/src/pages/PokemonDetails/PokemonDetails.js
@@ -4,21 +4,19 @@ import useRequestData from "../../hooks/useRequestData";
 import { BASE_URL } from "../../constants/urls";
 
 
-const PokemonDetails = (props) => {
-    const pokemon = useRequestData({}, `${BASE_URL}caterpie`)[0];
+const PokemonDetails = () => {
+    const pokemon = useRequestData({}, `${BASE_URL}caterpie`)[0]
 
-    console.log(pokemon)
-
-const statList = pokemon && pokemon.stats.map((statPoke) => {
-  return ( <ProgressBar  percent={statPoke.base_stat} statText={statPoke.stat.name} />)
-}) 
-return (
-    <div>
-        {pokemon && pokemon.name}
-        <img src={pokemon.sprites && pokemon.sprites.front_default} />
-
-    </div>
-)
+    const statList = pokemon.stats && pokemon.stats.map((statPoke) => {
+        return (<ProgressBar percent={statPoke.base_stat} statText={statPoke.stat.name} />)
+    })
+    return (
+        <div>
+            {pokemon && pokemon.name}
+            <img src={pokemon.sprites && pokemon.sprites.front_default} />
+            {statList}
+        </div>
+    )
 }
 
 
